5a66230efe294fd42d503ba58d052581d584fb3f
Aleksey_T <talekseys@mail.ru>
2023-05-21 12:43:24 +0300

    First commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $ru.netology.Urok10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
